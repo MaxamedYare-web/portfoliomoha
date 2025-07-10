5a791051ad2ec21b23a4ba79578ae915bb262131
Ibrahiim10 <ustadibrahim00@gmail.com>
2025-07-10 15:43:36 +0300

    Added navbar style

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,32 +1,35 @@
-import { FiMoon } from "react-icons/fi"
-import { NavLink } from "react-router-dom"
+import { FiMoon } from 'react-icons/fi';
+import { NavLink } from 'react-router-dom';
 
-
-export const HeaderPage = ()=>{
-
-const gColor = "#3865ec"
-const gColor2 = "#923ceb"
-
-
-return(<>
-
-<header className="flex justify-between items-center pt-4 lg:px-30">
-    <div className="div">
-        <h1 className={`text-4xl textGradient font-bold `}>Portfolio</h1>
-    </div>
-    <nav className="flex gap-4 text-2xl">
-    <NavLink>Home</NavLink>
-    <NavLink>About</NavLink>
-    <NavLink>Skills</NavLink>
-    <NavLink>Projects</NavLink>
-    <NavLink>Service</NavLink>
-    <NavLink>Blog</NavLink>
-    <NavLink>Resume</NavLink>
-    <NavLink>Contact</NavLink>
-    <NavLink>Admin</NavLink>
-    </nav>
-    <FiMoon />
-</header>
-
-</>)
-}
+export const HeaderPage = () => {
+  return (
+    <>
+      <header className="flex justify-between items-center pt-4 lg:px-30">
+        <div className="div">
+          <h1 className="text-4xl textGradient font-bold">Portfolio</h1>
+        </div>
+        <nav className="flex gap-4 text:sm font-semibold ">
+          <NavLink className="hover:text-blue-500 duration-500">Home</NavLink>
+          <NavLink className="hover:text-blue-500 duration-500">About</NavLink>
+          <NavLink className="hover:text-blue-500 duration-500">Skills</NavLink>
+          <NavLink className="hover:text-blue-500 duration-500">
+            Projects
+          </NavLink>
+          <NavLink className="hover:text-blue-500 duration-500">
+            Service
+          </NavLink>
+          <NavLink className="hover:text-blue-500 duration-500">
+            Service
+          </NavLink>
+          <NavLink className="hover:text-blue-500 duration-500">Blog</NavLink>
+          <NavLink className="hover:text-blue-500 duration-500">Resume</NavLink>
+          <NavLink className="hover:text-blue-500 duration-500">
+            Contact
+          </NavLink>
+          <NavLink className="hover:text-blue-500 duration-500">Admin</NavLink>
+        </nav>
+        <FiMoon />
+      </header>
+    </>
+  );
+};
